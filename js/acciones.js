acee19d105498b408904c8b19899db4f45333909
jorge luis padilla gonzalez <jorgeluispadillagonzalez@gmail.com>
2014-05-19 14:15:31 -0500

    uno 19/05/14
    primera subida de prueba01

---
+++ js/acciones.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$(document).ready(function() {
+	var Nombre;
+	
+	/*var fechaH=new Date();
+alert(fechaH);   
+var fechac=new Date("2014/04/10");
+alert(fechac); 
+result = fechac-fechaH
+dia = (((result/1000)/60)/60/24)
+alert (dia);*/
+$('#Mostrar').Click(function(e){
+	
+	
+		$('#Caracteristicas').text('Este animal sdvfjsbdfk.jbskdjfbksd');
+	
+	
+	
+		
+});
+	
+});//ready
